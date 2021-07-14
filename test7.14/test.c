18ae31f98538634a34855f16270de50aec897443
cheresh-all <86939892+cheresh-all@users.noreply.github.com>
2021-07-14 10:21:47 +0800

    添加项目文件。

---
+++ test7.14/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<string.h>
+#include<stdio.h>
+//int main()
+//{
+//	char arr1[] = "bit";
+//	char arr2[] = "########";
+//	strcpy(arr2, arr1);//strcpy 字符串拷贝
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+//int main()
+//{
+//	//memset  memory-set 内存-设置
+//	char arr1[] = "hello world";
+//	memset(arr1, '*', 5);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+//自定义函数
